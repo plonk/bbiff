baec8e3b60ed3cb8a0ef60a0e3ccd1e6d49da758
Yoteichi <plonk@piano.email.ne.jp>
2016-03-11 22:45:28 +0900

    res_format.rbのコードに対するテストを書いた。

---
+++ spec/res_format_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+# render_post だけテストすれば全部のメソッドをテストすることが可能だけ
+# ど、やっぱり全部のヘルパーメソッドにもテストを書いたほうがよさそう。
+
+describe "レスの整形をするメソッド群" do
+
+  it "render_nameが動く" do # ←英語風DSLの意味がない
+    expect(render_name("ナナシ", "sage")).to eq "ナナシ"
+  end
+
+  it "render_resnoが動く" do
+    expect(render_resno(1)).to eq "1"
+  end
+
+  it "render_dateが動く" do
+    expect(render_date(Time.at(0))).to eq "1970/1/1(木) 09:00:00"
+  end
+
+  it "indentが動く" do
+    expect(indent(0, "a")).to eq "a"
+    expect(indent(1, "a")).to eq " a"
+    expect(indent(1, "a\nb\n")).to eq " a\n b\n"
+  end
+
+  it "render_bodyが動く" do
+    expect(render_body("")).to eq "\n"
+    expect(render_body("<br>")).to eq "    \n\n"
+    expect(render_body("&65374;")).to eq "    &65374;\n"
+  end
+
+  it "render_postが動く" do
+    post = Bbs::Post.from_line("999<>名無しさん<>sage<>2011/11/11(金) 12:34:56<>ほげ<><>")
+    expect(render_post(post)).to eq "999：名無しさん：2011/11/11(金) 12:34:56\n    ほげ\n"
+  end
+end
